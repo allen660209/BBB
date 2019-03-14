56a52fbdee88eafa6708c97e1201bdfe42e67102
allen660209 <48303728+allen660209@users.noreply.github.com>
2019-03-14 10:38:01 +0800

    Add files via upload

---
+++ all.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+
+$(document).ready(function () {
+    $('h1').click(function (event) {
+        $('p1').slideToggle();
+    });
+    
+    //$('p1').hide();
+    //$('#title').hide();
+
+});
